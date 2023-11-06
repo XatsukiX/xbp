3cd1f198a7bbf9e1a1e1b46e6e68048951c53a49
XatsukiX <r202301989ko@jindai.jp>
2023-11-07 00:30:32 +0900

    20231106

--- de12/python/test2.py
+++ de12/python/test2.py
@@ -1,8 +1,27 @@
-import requests
-import json
+from pyowm.owm import OWM
+from pyowm.utils import formatting
+from pyowm.utils.config import get_default_config
 
-url="https://api.openweathermap.org/data/2.5/forecast?q={city}&appid{key}&lang=ja&units=metric"
-url= url.format(city="Yokohama,JP",key="bad5e934d78a420d7cae96a961a93508")
+#  PyOWMのコンフィグ設定
+config_dict = get_default_config()
+config_dict["language"] = "ja" # 取得データの言語設定
 
-jsondata = requests.get(url).json()
-print(jsondata)
+# PyOWMライブラリの初期化
+owm = OWM("bad5e934d78a420d7cae96a961a93508", config_dict)
+mgr = owm.weather_manager()
+
+# 現在の気象データを取得
+observation = mgr.weather_at_place("Yokohama,JP")
+
+w = observation.weather
+print("気象データの計測日次時間(date): {}".format(formatting.to_date(w.ref_time)))
+print("天気: {}".format(w.status))
+print("天気詳細: {}".format(w.detailed_status))
+print("気温(℃): {}".format(w.temperature("celsius")))
+print("湿度(%): {}".format(w.humidity))
+print("気圧(hPa): {}".format(w.barometric_pressure()))
+print("風: {}".format(w.wind()))
+
+print("雲量: {}".format(w.clouds))
+print("雨量: {}".format(w.rain))
+print("積雪量: {}".format(w.snow))
