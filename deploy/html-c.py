42e981de82eedbba8d5b47aa362c41dd46ae8f28
XatsukiX <r202301989ko@jindai.jp>
2024-06-20 14:24:28 +0900

    20240620

---
+++ deploy/html-c.py
@@ -0,0 +1,82 @@
+html = """<!DOCTYPE html>
+<!-- saved from url=(0075)https://template-party.com/template/tp_biz60/tp_biz60_bg_black/company.html -->
+<html lang="ja"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>PROJECT:BUILD</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body class="p">
+
+<div id="container">
+
+<header>
+
+<div id="logo">
+<h1><a href="">Note</a></h1>
+</div>
+
+</header>
+
+<main>
+
+<section>
+
+<h2 class="linestyle"><span class="uline">PROJECT NOTE</span></h2>
+
+<table class="ta1">
+<caption>プロジェクト名</caption>
+  <tbody>
+  <tr>
+    <th>プロジェクト概要</th>
+    <td></td>
+  </tr>
+  <tr>
+    <th>使用ツール</th>
+    <td>
+      <ul>
+        <li>ツール</li>
+      </ul>
+    </td>
+  </tr>
+  </tbody>
+</table>
+
+</section>
+
+
+<div class="c2">
+
+<div class="main">
+
+
+
+
+</main>
+
+<footer>
+
+<div class="copy">
+<small>Copyright© All Rights Reserved.</small>
+</div>
+
+</footer>
+
+
+
+</div>
+<!--/#container-->
+
+
+</body></html>
+
+"""
+
+
+
+
+with open("name.html", "w", encoding="utf-8") as f:
+    f.write(html)
+
+
